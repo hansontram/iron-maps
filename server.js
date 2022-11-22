40e1865513617c59b93f63a65c543b9cdc782382
hansontram <htram.dev@gmail.com>
2022-11-22 11:17:40 -0800

    PORT varaible

--- server.js
+++ server.js
@@ -61,6 +61,6 @@ app.use("/gym", gymRoutes);
 app.use("/review", reviewRoutes);
 
 //Server Running
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 'PORT' , () => {
   console.log("Server is running, you better catch it!");
 });
